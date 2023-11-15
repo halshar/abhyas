68a1ed5b2bd60226cbc863c88aa47dc5d33c1188
Harshal Jadhav <bestt1315@gmail.com>
2023-11-15 05:41:34 +0530

    fixed return types and typos

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use rusqlite::ffi::{SQLITE_CONSTRAINT_PRIMARYKEY, SQLITE_CONSTRAINT_UNIQUE};
 use rusqlite::Connection;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
-enum CustomErros {
+enum CustomErrors {
     CacheDirectoryNotFound,
     CreateDirectoryFailed,
     CreateDBFileFailed,
@@ -25,7 +25,7 @@ impl Db {
     }
 
     /// add new links into the db
-    fn add_link(&self, link: String) -> Result<(), CustomErros> {
+    fn add_link(&self, link: String) -> Result<(), CustomErrors> {
         match self.conn.execute(
             "INSERT INTO links (link,solved_count,is_solved,is_skipped) VALUES (?1,?2,?3,?4)",
             (&link, 0, 0, 0),
@@ -37,15 +37,15 @@ impl Db {
                         && (err.extended_code == SQLITE_CONSTRAINT_PRIMARYKEY
                             || err.extended_code == SQLITE_CONSTRAINT_UNIQUE)
                     {
-                        return Err(CustomErros::DuplicateLinkValue);
+                        return Err(CustomErrors::DuplicateLinkValue);
                     } else {
-                        return Err(CustomErros::Others(
+                        return Err(CustomErrors::Others(
                             "Error: Something went wrong while inserting link".to_owned(),
                         ));
                     }
                 }
                 _ => {
-                    return Err(CustomErros::Others(
+                    return Err(CustomErrors::Others(
                         "Error: Something went wrong while inserting link".to_owned(),
                     ))
                 }
@@ -55,36 +55,36 @@ impl Db {
 }
 
 /// create directory to store the db file
-fn create_file() -> Result<(), CustomErros> {
+fn create_file() -> Result<PathBuf, CustomErrors> {
     let cache_dir = match dirs::cache_dir() {
         Some(value) => value,
-        None => return Err(CustomErros::CacheDirectoryNotFound),
+        None => return Err(CustomErrors::CacheDirectoryNotFound),
     };
 
     let dir_name = &cache_dir.join("abhyas");
     if Path::new(&dir_name).try_exists().is_err() {
         if fs::create_dir(&dir_name).is_err() {
-            return Err(CustomErros::CreateDirectoryFailed);
+            return Err(CustomErrors::CreateDirectoryFailed);
         }
     }
 
     let file_name = &dir_name.join("abhyas.db");
     if Path::new(&file_name).try_exists().is_err() {
         if fs::File::create(&file_name).is_err() {
-            return Err(CustomErros::CreateDBFileFailed);
+            return Err(CustomErrors::CreateDBFileFailed);
         }
     }
 
-    Ok(())
+    Ok(file_name.to_path_buf())
 }
 
 /// create db connection
-fn create_db_connection() -> Result<Connection, CustomErros> {
+fn create_db_connection() -> Result<Connection, CustomErrors> {
     let file_name = create_file()?;
 
     let conn = match Connection::open(file_name) {
         Ok(value) => value,
-        Err(e) => return Err(CustomErros::DBConnectionFailed),
+        Err(_) => return Err(CustomErrors::DBConnectionFailed),
     };
 
     if conn
@@ -99,40 +99,41 @@ fn create_db_connection() -> Result<Connection, CustomErros> {
         )
         .is_err()
     {
-        return Err(CustomErros::DBQueryFailed);
+        return Err(CustomErrors::DBQueryFailed);
     }
 
     Ok(conn)
 }
 
-fn main() -> rusqlite::Result<()> {
-    let _conn = Db::new(create_db_connection().unwrap());
-
-    _conn.add_link("demo".to_owned())?;
-
-    Ok(())
-}
-
 fn main() {
-    let _conn = match create_db_connection() {
+    let _conn: Connection = match create_db_connection() {
         Ok(value) => value,
         Err(e) => match e {
-            CustomErros::CacheDirectoryNotFound => {
-                println!("Error: The cache directory was not found")
+            CustomErrors::CacheDirectoryNotFound => {
+                println!("Error: The cache directory was not found");
+                return;
             }
-            CustomErros::CreateDirectoryFailed => {
-                println!("Error: Couldn't create the db directory")
+            CustomErrors::CreateDirectoryFailed => {
+                println!("Error: Couldn't create the db directory");
+                return;
             }
-            CustomErros::CreateDBFileFailed => println!("Error: Couldn't create the db file"),
-            CustomErros::DBConnectionFailed => println!("Error: DB connection failed"),
-            CustomErros::DBQueryFailed => println!("Error: DB query failed"),
+            CustomErrors::CreateDBFileFailed => {
+                println!("Error: Couldn't create the db file");
+                return;
+            }
+            CustomErrors::DBConnectionFailed => {
+                println!("Error: DB connection failed");
+                return;
+            }
+            CustomErrors::DBQueryFailed => {
+                println!("Error: DB query failed");
+                return;
+            }
             _ => unreachable!(),
         },
     };
 
     let db = Db::new(_conn);
-
-    show_options(db);
 }
 
 #[cfg(test)]
