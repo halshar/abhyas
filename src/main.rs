2156da7dcb072b1012b99c11c53740fde0c12f3e
Harshal Jadhav <bestt1315@gmail.com>
2023-11-16 05:27:59 +0530

    added interactive prompt to add new links

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use inquire::{required, validator::Validation, Select, Text};
 use rusqlite::ffi::{SQLITE_CONSTRAINT_PRIMARYKEY, SQLITE_CONSTRAINT_UNIQUE};
 use rusqlite::Connection;
 use std::fs::{self, OpenOptions};
 use std::path::PathBuf;
 
+#[derive(Debug)]
 enum CustomErrors {
     CacheDirectoryNotFound,
     CreateDirectoryFailed,
@@ -10,7 +12,17 @@ enum CustomErrors {
     DBConnectionFailed,
     DBQueryFailed,
     DuplicateLinkValue,
+    StatementFailed,
+    InvalidColumnName(String),
+    OperationCanceled,
+    OperationInterrupted,
     Others(String),
+    Exit,
+}
+
+enum UserOptions {
+    AddLink,
+    Exit,
 }
 
 /// struct to carry the db connection
@@ -51,6 +63,33 @@ impl Db {
                 }
             },
         }
+    }
+
+    fn get_links(&self) -> Result<Vec<String>, CustomErrors> {
+        let mut stmt = match self.conn.prepare("SELECT link FROM links") {
+            Ok(val) => val,
+            Err(_) => return Err(CustomErrors::StatementFailed),
+        };
+
+        let rows = match stmt.query_map([], |row| row.get(0)) {
+            Ok(val) => val,
+            Err(e) => match e {
+                rusqlite::Error::InvalidColumnName(e) => {
+                    return Err(CustomErrors::InvalidColumnName(e))
+                }
+                _ => {
+                    return Err(CustomErrors::Others(
+                        "Error: Something went wrong while reading all links".to_owned(),
+                    ))
+                }
+            },
+        };
+
+        let mut names: Vec<String> = Vec::new();
+        for name_result in rows {
+            names.push(name_result.unwrap())
+        }
+        Ok(names)
     }
 }
 
@@ -107,6 +146,79 @@ fn create_db_connection() -> Result<Connection, CustomErrors> {
     Ok(conn)
 }
 
+fn show_options(db: Db) -> Result<(), CustomErrors> {
+    let options = vec!["Add Link", "Exit"];
+
+    let user_option = match Select::new("select your option", options).prompt() {
+        Ok(val) => val,
+        Err(e) => match e {
+            inquire::InquireError::OperationCanceled => {
+                return Err(CustomErrors::OperationCanceled)
+            }
+            inquire::InquireError::OperationInterrupted => {
+                return Err(CustomErrors::OperationInterrupted)
+            }
+            _ => {
+                return Err(CustomErrors::Others(
+                    "Error: Unable to show the select menu".to_owned(),
+                ))
+            }
+        },
+    };
+
+    let selected_item = match user_option {
+        "Add Link" => UserOptions::AddLink,
+        "Exit" => UserOptions::Exit,
+        _ => unreachable!(),
+    };
+
+    match selected_item {
+        UserOptions::AddLink => user_link_input(&db)?,
+        UserOptions::Exit => return Err(CustomErrors::Exit),
+    }
+
+    Ok(())
+}
+
+fn user_link_input(db: &Db) -> Result<(), CustomErrors> {
+    let links = db.get_links()?;
+    let validator = move |input: &str| {
+        if links.contains(&input.to_owned()) {
+            Ok(Validation::Invalid(
+                "Duplicate link, enter another link".into(),
+            ))
+        } else {
+            Ok(Validation::Valid)
+        }
+    };
+
+    let link = match Text::new("Enter the link:")
+        .with_validator(required!())
+        .with_validator(validator)
+        .with_help_message("enter the link you want to save and hit enter")
+        .prompt()
+    {
+        Ok(val) => val,
+        Err(e) => match e {
+            inquire::InquireError::OperationCanceled => {
+                return Err(CustomErrors::OperationCanceled)
+            }
+            inquire::InquireError::OperationInterrupted => {
+                return Err(CustomErrors::OperationInterrupted)
+            }
+            _ => {
+                return Err(CustomErrors::Others(
+                    "Error: Something went wrong while taking link input".to_owned(),
+                ))
+            }
+        },
+    };
+
+    _ = db.add_link(link);
+
+    Ok(())
+}
+
 fn main() {
     let _conn: Connection = match create_db_connection() {
         Ok(value) => value,
@@ -136,64 +248,27 @@ fn main() {
     };
 
     let db = Db::new(_conn);
-}
 
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn check_create_file() {
-        let output = create_file();
-        assert!(output.is_ok());
-    }
-
-    #[test]
-    fn check_db_connection() {
-        let output = create_db_connection();
-        assert!(output.is_ok())
-    }
-
-    fn create_test_db(path: String) -> Db {
-        let conn = Connection::open(&path).unwrap();
-
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS links (
-                link            TEXT PRIMARY KEY,
-                solved_count    INTEGER NOT NULL,
-                is_solved       INTEGER NOT NULL,
-                is_skipped      INTEGER NOT NULL
-            )",
-            (),
-        )
-        .unwrap();
-
-        Db { conn }
-    }
-
-    fn delete_test_db(path: String) {
-        let path = PathBuf::from(path);
-
-        if path.exists() {
-            fs::remove_file(&path).expect("Failed to remove the test db file");
-        }
-    }
-
-    #[test]
-    fn check_unique_insert() {
-        let path = String::from("unique.db");
-        let conn = create_test_db(path.clone());
-        assert!(conn.add_link("https://test.com".to_owned()).is_ok());
-        delete_test_db(path);
-    }
-
-    #[test]
-    fn check_duplicate_insert() {
-        let path = String::from("duplicate.db");
-        let conn = create_test_db(path.clone());
-        conn.add_link("https://test.com".to_owned()).unwrap();
-        assert!(conn.add_link("https://test.com".to_owned()).is_err());
-        delete_test_db(path);
+    match show_options(db) {
+        Ok(_) => (),
+        Err(e) => match e {
+            CustomErrors::DuplicateLinkValue => {
+                println!("Error: Link already exists, input other link")
+            }
+            CustomErrors::StatementFailed => println!("Error: Failed to execute the statement"),
+            CustomErrors::InvalidColumnName(column_name) => {
+                println!("Error: column {} does not exist", column_name)
+            }
+            CustomErrors::OperationCanceled => {
+                println!("Error: User cancelled the operation")
+            }
+            CustomErrors::OperationInterrupted => {
+                println!("Error: User forcefully quit the operation")
+            }
+            CustomErrors::Others(msg) => println!("Error: {}", msg),
+            CustomErrors::Exit => println!("You've sucessfully quit the application :)"),
+            _ => unreachable!(),
+        },
     }
 }
+
